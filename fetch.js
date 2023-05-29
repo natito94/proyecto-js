ad5ebe86f337c5f187ebffd51edd84ccded53fcd
natalia <oggiani.nat@outlook.com>
2023-05-29 20:27:39 -0300

    api

---
+++ fetch.js
@@ -0,0 +1,77 @@
+// Obtener una referencia al elemento <div> con el id "integrantes"
+const integrantesS = document.getElementById('integrantesS');
+let characters = []
+// Realizar la solicitud HTTP a la API
+fetch('https://hp-api.onrender.com/api/characters/house/Slytherin')
+.then(respuesta=>respuesta .json())
+.then((datos) => {
+  characters = [...datos]
+  mostrarS(characters)
+})
+.catch(error => console.log(error))
+
+function mostrarS (datos){
+    datos.forEach(character => {
+     const li = document.createElement("li");
+     li.textContent = ` ${character.name}`
+     integrantesS.appendChild(li)
+    })
+}
+
+
+const integrantesG = document.getElementById('integrantesG');
+let charactersG = []
+// Realizar la solicitud HTTP a la API
+fetch('https://hp-api.onrender.com/api/characters/house/gryffindor')
+.then(respuesta=>respuesta .json())
+.then((datos) => {
+  charactersG = [...datos]
+  mostrarG(charactersG)
+})
+.catch(error => console.log(error))
+
+function mostrarG (datos){
+    datos.forEach(character => {
+     const li = document.createElement("li");
+     li.textContent = ` ${character.name}`
+     integrantesG.appendChild(li)
+    })         
+}
+
+const integrantesH = document.getElementById('integrantesH');
+let charactersH = []
+// Realizar la solicitud HTTP a la API
+fetch('https://hp-api.onrender.com/api/characters/house/hufflepuff')
+.then(respuesta=>respuesta .json())
+.then((datos) => {
+  charactersH = [...datos]
+  mostrarH(charactersH)
+})
+.catch(error => console.log(error))
+
+function mostrarH (datos){
+    datos.forEach(character => {
+     const li = document.createElement("li");
+     li.textContent = ` ${character.name}`
+     integrantesH.appendChild(li)
+    })         
+}
+
+const integrantesR = document.getElementById('integrantesR');
+let charactersR = []
+// Realizar la solicitud HTTP a la API
+fetch('https://hp-api.onrender.com/api/characters/house/Ravenclaw')
+.then(respuesta=>respuesta .json())
+.then((datos) => {
+  charactersR = [...datos]
+  mostrarR(charactersR)
+})
+.catch(error => console.log(error))
+
+function mostrarR (datos){
+    datos.forEach(character => {
+     const li = document.createElement("li");
+     li.textContent = ` ${character.name}`
+     integrantesR.appendChild(li)
+    })         
+}
